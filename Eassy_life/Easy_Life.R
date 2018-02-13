f19f036492ba7ed933f043045b78f7e3a51ab88e
jfores <fores.martos.jaume@gmail.com>
2018-02-13 21:21:16 +0100

    added plotting functions in WGCNA
    JKJK

--- Eassy_life/Easy_Life.R
+++ Eassy_life/Easy_Life.R
@@ -1,6 +1,13 @@
 #Function that transform NA of a vector to UNDET string value.
 
 NA_To_Undet <- function(vec){
-	vec[is.na(vec) | vec == "NA"] <- "UNDET"
+	vec[is.na(vec) | vec == "NA" | vec == "N/A"] <- "UNDET"
 	return(vec)
 }
+
+Sp_To_UnSc <- function(vec){
+	vec <- gsub(" ","_",vec)
+	return(vec)
+}
+
+

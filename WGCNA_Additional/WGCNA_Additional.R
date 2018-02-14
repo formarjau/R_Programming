bf542bd99562ad8ab83efe21f90b659687042149
jfores <fores.martos.jaume@gmail.com>
2018-02-14 01:05:53 +0100

    mor

--- WGCNA_Additional/WGCNA_Additional.R
+++ WGCNA_Additional/WGCNA_Additional.R
@@ -176,15 +176,17 @@ enrich_with_fgsea <- function(corr_list,pathway){
   return(fgseaRes)
   }
 
+
+# Perform Reactome Enrichment in all the modules create plots and save results.
+
  perform_enrichment_in_all_modules <- function(exprs,colors,MEs,directory){
  	library(org.Hs.eg.db)
 	library(annotate)
 	library(ReactomePA)
-
 	geneModuleMembership = as.data.frame(cor(exprs, MEs, use = "p"));
 	modNames = substring(names(MEs), 3);
 	names(geneModuleMembership) = paste("MM", modNames, sep="");
-	gene_module_assign <- data.frame(colcrs,colnames(exprs),unlist(lookUp(colnames(exprs), 'org.Hs.eg', 'SYMBOL')),row.names = colnames(exprs));
+	gene_module_assign <- data.frame(colors,colnames(exprs),unlist(lookUp(colnames(exprs), 'org.Hs.eg', 'SYMBOL')),row.names = colnames(exprs));
 	colnames(gene_module_assign) <- c("Color","Entrez","Symbol");
 	list_modules <- list()
 	colors <- unique(gene_module_assign$Color)
@@ -192,13 +194,18 @@ enrich_with_fgsea <- function(corr_list,pathway){
 		genes <- gene_module_assign[gene_module_assign$Color == colors[i],]
 		paths <- enrichPathway(gene=genes$Entrez,pvalueCutoff=0.05, readable=T)
 		paths_df <- data.frame(paths) 
-		pdf(file=paste(directory,colors[i],"_dotplot_.pdf",sep=""),width = 15)
-		dotplot(x, showCategory=40)
+		try({
+		pdf(file=paste(directory,colors[i],"_dotplot.pdf",sep=""),width = 15,height = 15)
+		print(dotplot(paths,showCategory=20))
 		dev.off()
-		pdf(file=paste(directory,colors[i],"_enrichMap_.pdf",sep=""),width = 15)
-		enrichMap(x, layout=igraph::layout.kamada.kawai, vertex.label.cex = 0.8,n = 40)
-		dev.off()
+		pdf(file=paste(directory,colors[i],"_enrichMap.pdf",sep=""),width = 15,height = 15)
+		enrichMap(paths, layout=igraph::layout.kamada.kawai, vertex.label.cex = 0.8,n = 40)
+		list_temp <- list(genes,paths,paths_df)
+		dev.off()})
+		print(colors[i])
+		list_modules[[colors[i]]] <- list_temp
 	}
+	return(list_modules)
  }
 
 ############################################
@@ -237,3 +244,95 @@ enrich_with_enrichr <- function(list_of_genes){
   return(query)
   }
 
+
+##############################
+#PERFORMING GSEA FUNCTIONS####
+##############################
+
+#Step one analysis
+
+perform_Step_One <- function(exprs,directory,tagg,threshold_fit = 0.85,minModuleSize = 30){
+	print("Selecting Power...")
+	powers = c(c(1:20), seq(from = 12, to=20, by=2))
+	sft = pickSoftThreshold(exprs, powerVector = powers, verbose = 5)
+	save(file=paste(directory,tagg,"_soft_thresh.Rda"),sft)
+	softPower <- sft$fitIndices[sft$fitIndices$SFT.R.sq > threshold_fit,][1,1]
+	print(softPower)
+	print("Computing adjacency matrix...")
+	adjacency = adjacency(exprs, power = softPower);
+	save(file=paste(directory,tagg,"_adjacency_Healthy.Rda"),adjacency)
+	print("Computing TOM...")
+	TOM = TOMsimilarity(adjacency);
+	save(file=paste(directory,tagg,"_TOM_Healthy.Rda"),TOM)
+	dissTOM = 1-TOM
+	geneTree = hclust(as.dist(dissTOM), method = "average");
+	minModuleSize = minModuleSize;
+	dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM, deepSplit = 2, pamRespectsDendro = FALSE, minClusterSize = minModuleSize);
+	dynamicColors = labels2colors(dynamicMods)
+	number_of_modules <- length(table(dynamicColors))
+	number_of_genes_in_modules <- table(dynamicColors)
+
+	pdf(file = paste(directory,tagg,"_geneTree_Mod_Cols.pdf"))
+	plotDendroAndColors(geneTree, dynamicColors, "Dynamic Tree Cut",dendroLabels = FALSE, hang = 0.03, addGuide = TRUE, guideHang = 0.05, main = "Gene dendrogram and module colors")
+	dev.off()
+	print("Computing Module eigengenes...")
+	MEList = moduleEigengenes(exprs, colors = dynamicColors)
+	MEs = MEList$eigengenes
+	MEDiss = 1-cor(MEs);
+	METree = hclust(as.dist(MEDiss), method = "average");
+	pdf(file = paste(directory,tagg,"_METree.pdf"))
+	plot(METree, main = "Clustering of module eigengenes",xlab = "", sub = "")
+	MEDissThres = 0.25
+	abline(h=MEDissThres, col = "red")
+	dev.off()
+	save(exprs,softPower,adjacency,TOM,dissTOM,geneTree,minModuleSize,dynamicColors,number_of_modules,number_of_genes_in_modules,MEList,MEs,MEDiss,METree,file= paste(directory,tagg,"Step_1_Data.Rdata",sep=""))
+}
+
+#Step two ananlysis
+
+perform_Step_Two <- function(exprs,dynamicColors,tagg,MEDissThres = 0.25,directory){
+	print("Merging close modules...")
+	merge = mergeCloseModules(exprs, dynamicColors, cutHeight = MEDissThres, verbose = 3)
+	pdf(file=paste(directory,tagg,"Module_Gene_Content.pdf"))
+	barplot(table(merge$colors),col = names(table(merge$colors)),cex.names = .7,las = 2)
+	dev.off()
+	mergedColors = merge$colors;
+	mergedMEs = merge$newMEs;
+	pdf(file =paste(directory,tagg,"_Tree_Merged_Colors.pdf"))
+	plotDendroAndColors(geneTree, cbind(dynamicColors, mergedColors),c("Dynamic Tree Cut", "Merged dynamic"),dendroLabels = FALSE, hang = 0.03,addGuide = TRUE, guideHang = 0.05)
+	dev.off()
+	moduleColors = mergedColors
+	colorOrder = c("grey", standardColors(50));
+	moduleLabels = match(moduleColors, colorOrder)-1;
+	MEs = mergedMEs;
+	save(merge,mergedColors,mergedMEs,moduleColors,colorOrder,moduleLabels,MEs,file=paste(directory,tagg,"Step_2_Data.Rdata",sep="\t"))
+}
+
+perform_Step_Three <- function(exprs,directory,tagg,dat_traits,moduleColors){
+	nGenes = ncol(exprs);
+	nSamples = nrow(exprs);
+	datTraits = dat_traits
+	MEs0 = moduleEigengenes(exprs, moduleColors)$eigengenes
+	MEs = orderMEs(MEs0)
+	moduleTraitCor = cor(MEs, datTraits, use = "p");
+	moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples);
+	save(nGenes,nSamples,datTraits,MEs0,MEs,moduleTraitCor, moduleTraitPvalue, file = paste(directory,tagg,"Step_3_Data.Rdata"))
+	pdf(file = 	paste(directory,tagg,"Module_Trait_Associations.pdf"))
+	textMatrix = paste(signif(moduleTraitCor, 2), "\n(",
+	signif(moduleTraitPvalue, 1), ")", sep = "");
+	dim(textMatrix) = dim(moduleTraitCor)
+	par(mar = c(6, 8.5, 3, 3));
+	# Display the correlation values within a heatmap plot
+	labeledHeatmap(Matrix = moduleTraitCor,
+	xLabels = names(datTraits),
+	yLabels = names(MEs),
+	ySymbols = names(MEs),
+	colorLabels = FALSE,
+	colors = greenWhiteRed(50),
+	textMatrix = textMatrix,
+	setStdMargins = FALSE,
+	cex.text = 0.5,
+	zlim = c(-1,1),
+	main = paste("Module-trait relationships"))
+	dev.off()
+	}
